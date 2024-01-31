2e41f275b8442363034aada0971cbd1836b5cc9e
opilane <opilane@HP-CZC2349HTZ>
2024-01-31 11:43:23 +0200

    Add project files.

---
+++ Kontrolltöö Kordused/Kontrolltöö_Kordused.py
@@ -0,0 +1,22 @@
+﻿#1
+#n=int(input("1 kuni 9:"))
+#for i in range(n):
+#    print(" ~~~~~")
+#    print("/_____\ ")
+#    print("| []  |")
+#    print(" -----")
+#3﻿
+õpilased = 30 
+maksimaalne = 5
+minimmalne = 1
+for i in range(õpilased):
+    hinnad = random.randint(100,500)/100
+    print(hinned)
+    if hinned > maksimaalne:
+        maksimalne = hinned
+    if hinned < minimaalne:
+        minimaalne = hinned
+print("Maksimaalne hind on:", maksimaalne)
+print("Minimaalne hind on:", minimaalne)
+ 
+ 
