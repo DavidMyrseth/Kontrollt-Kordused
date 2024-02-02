06aaef1c1891814dd7fa481ccdb010b70c35f47a
opilane <opilane@HP-CZC2349HTF>
2024-02-02 10:47:49 +0200

    - brain 9

--- Kontrolltöö Kordused/Kontrolltöö_Kordused.py
+++ Kontrolltöö Kordused/Kontrolltöö_Kordused.py
@@ -1,71 +1,71 @@
-﻿##1
-#import random
+﻿#1
+import random
 
 
-#n=int(input("1 kuni 9: "))
-#for i in range(n):
-#    print(" ~~~~~")
-#    print("/_____\ ")
-#    print("| []  |")
-#    print(" -----")
+n=int(input("1 kuni 9: "))
+for i in range(n):
+    print(" ~~~~~")
+    print("/_____\ ")
+    print("| []  |")
+    print(" -----")
 
-##2
-#while True:
-#    try:
-#            o=int(input("Sisesta õpilaste arv ühes klassis: "))
-#            o=o*2
-#            s=0
-#            for x in range(1, o+1):
-#                h=random.randint(1,100)
-#                print(f"Õpilane {x}: {h}")
-#                s=s+h
-#            s=s/o
-#            print(f"Keskmine hinnang: {s}")
-#            break
-#    except:
-#        print("Vale Andmetüüp")
-##3﻿
-#õpilased = 30 
-#maksimaalne = 5
-#minimmalne = 1
-#for i in range(õpilased):
-#    hinned = random.randint(100,500)/100
-#    print(hinned)
-#    if hinned > maksimaalne:
-#        maksimalne = hinned
-#    elif hinned < minimaalne:
-#        minimaalne = hinned
-#print("Maksimaalne hind on:", maksimaalne)
-#print("Minimaalne hind on:", minimaalne)
+#2
+while True:
+    try:
+            o=int(input("Sisesta õpilaste arv ühes klassis: "))
+            o=o*2
+            s=0
+            for x in range(1, o+1):
+                h=random.randint(1,100)
+                print(f"Õpilane {x}: {h}")
+                s=s+h
+            s=s/o
+            print(f"Keskmine hinnang: {s}")
+            break
+    except:
+        print("Vale Andmetüüp")
+#3﻿
+õpilased = 30 
+maksimaalne = 5
+minimmalne = 1
+for i in range(õpilased):
+    hinned = random.randint(100,500)/100
+    print(hinned)
+    if hinned > maksimaalne:
+        maksimalne = hinned
+    elif hinned < minimaalne:
+        minimaalne = hinned
+print("Maksimaalne hind on:", maksimaalne)
+print("Minimaalne hind on:", minimaalne)
 
-##4
-#s2=0
-#n2=0
-#for x in range(1,13):
-#    S=random.randint(5,10)
-#    n=random.randint(20000,50000)
-#    print(f"Maakond {x}:\nElanikud: {n}\nVäljak: {S} km2")
-#    print()
-#    s2=s2+S
-#    n2=n2+n
-#p=n2//s2
-#print(f"Piirkondlik rahvastikutihedus: {p}")
+#4
+s2=0
+n2=0
+for x in range(1,13):
+    S=random.randint(5,10)
+    n=random.randint(20000,50000)
+    print(f"Maakond {x}:\nElanikud: {n}\nVäljak: {S} km2")
+    print()
+    s2=s2+S
+    n2=n2+n
+p=n2//s2
+print(f"Piirkondlik rahvastikutihedus: {p}")
 
-##5
-#while True:
-#    try:
-#        min=int(input("Sisesta minimaalne väärtus: "))
-#        max=int(input("Sisesta maksimaalne väärtus: "))
-#        s=int(input("Sisesta samm: "))
-#        print("y=-0.5x+x")
-#        print()
-#        for x in range(min, max, s):
-#            y=-0.5*x+x
-#            print(f"x={x}\ny={y}")
-#            print()
-#        break
-#    except:
-#        print("Vale Andmetüüp") 
+#5
+while True:
+    try:
+        min=int(input("Sisesta minimaalne väärtus: "))
+        max=int(input("Sisesta maksimaalne väärtus: "))
+        s=int(input("Sisesta samm: "))
+        print("y=-0.5x+x")
+        print()
+        for x in range(min, max, s):
+            y=-0.5*x+x
+            print(f"x={x}\ny={y}")
+            print()
+        break
+    except:
+        print("Vale Andmetüüp") 
 
 #1.1
 #while True:
@@ -114,28 +114,28 @@
 #print(f"Keskmine hinne 2 klassis on {kesk2}")
 
 #5
-from random import *
+#from random import *
 
 
-while True:
-    try:
-        k=int(input("Mitu kotleti sul on? "))
-        if k>0: break
-    except ValueError:
-        print("Vale tüüp")
-while True:
-    try:
-        m=int(input("Mitu kotleti ühel pannil? "))
-        if m>0: break
-    except ValueError:
-        print("Vale tüüp")
-pann=0
-lisapann=0
-while k>0:
-    if k>=m:
-        k-=m
-        pann+=1
-    elif k<m:
-        lisapann+=1
-print(f"Täispannid: {pann} ja veel on vaja {lisapann}")
-print()
+#while True:
+#    try:
+#        k=int(input("Mitu kotleti sul on? "))
+#        if k>0: break
+#    except ValueError:
+#        print("Vale tüüp")
+#while True:
+#    try:
+#        m=int(input("Mitu kotleti ühel pannil? "))
+#        if m>0: break
+#    except ValueError:
+#        print("Vale tüüp")
+#pann=0
+#lisapann=0
+#while k>0:
+#    if k>=m:
+#        k-=m
+#        pann+=1
+#    elif k<m:
+#        lisapann+=1
+#print(f"Täispannid: {pann} ja veel on vaja {lisapann}")
+#print()
